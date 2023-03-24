e8842f77d7ed9d108f7f0db3f650ac75372b2fe4
Adam Cerha <a.cerha@email.cz>
2023-03-24 10:34:13 +0100

    Added arduino code

---
+++ arduino/buttons_serial/buttons_serial.ino
@@ -0,0 +1,40 @@
+#include <SoftwareSerial.h>
+
+int buttonPins[5];
+int byt[8];
+byte output;
+void setup() {
+Serial.begin(9600);
+for (int i = 0; i < 5; i++){
+  buttonPins[i] = i+8;
+  pinMode(buttonPins[i], INPUT);
+  }
+
+}
+void loop() {
+
+
+for (int i = 0; i < 5; i++){
+  if(digitalRead(buttonPins[i]) == HIGH) {
+  byt[i] = 1;}
+  else {byt[i] = 0;}
+  }
+output = 0;
+int power = 1;
+for (int i = 0; i < 8; i++){
+  power = 1;
+  for(int j = 0; j < i; j++){
+    power = power *2;
+    }
+
+output += (byt[i] * (power));
+ 
+}
+Serial.println(char(output));
+
+  //Serial.println(output);
+//Serial.println(digitalRead(sensorPin));
+//message to the receiving device
+
+delay(100);
+}
