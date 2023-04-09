81e9599c9bbf9048e80fdcd08da5a9a2005589fc
Adam Cerha <a.cerha@email.cz>
2023-04-09 13:04:44 +0200

    Add loop function to wrapper

--- start.bat
+++ start.bat
@@ -5,15 +5,16 @@ Start /wait Test.exe
 
 cd ../../../
 
+set /A loops=2
 
-
+for /l %%x in (1, 1, %loops%) do (
+:: One year loop from Mining to Auction
+cls
 echo Press any key to start mining...
 pause >nul
 cd Processing/Mining/windows-amd64/
 Start /wait Mining.exe
-
 cd ../../../
-
 
 cls
 echo Press any key to start save editor...
@@ -36,11 +37,4 @@ pause>nul
 cd Processing/Auction/windows-amd64/
 Start /wait Auction.exe
 cd ../../../
-
-cls
-echo Press any key to start mining...
-pause>nul
-cd Processing/Mining/windows-amd64/
-Start /wait Mining.exe
-cd ../../../
-
+)
